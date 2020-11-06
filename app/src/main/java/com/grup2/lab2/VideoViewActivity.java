acf99fdd1ed1d48b690de7dec2f3b8e781417275
robertvala <robavala@espol.edu.ec>
2020-11-06 11:38:36 -0500

    Creacion del video view activity

---
+++ app/src/main/java/com/grup2/lab2/VideoViewActivity.java
@@ -0,0 +1,14 @@
+package com.grup2.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideoViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_view);
+    }
+}
